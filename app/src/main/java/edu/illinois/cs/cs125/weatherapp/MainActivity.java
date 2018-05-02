1afe2d073ce02924a4039001277f068b6d1cf74c
Jasmine.W <jw9@illinois.edu>
2018-05-02 15:43:50 -0500

    123 night mode

--- app/src/main/java/edu/illinois/cs/cs125/weatherapp/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/weatherapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
@@ -13,6 +14,8 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -33,7 +36,7 @@ import static android.webkit.ConsoleMessage.MessageLevel.LOG;
 public class MainActivity extends AppCompatActivity implements LocationListener {
 
 
-    TextView txtCity, txtLastUpdate, txtDescription, txtHumidity, txtCelsius, txtTime;
+    TextView txtCity, txtLastUpdate, txtDescription, txtHumidity, txtCelsius, txtTime, txtLat;
     ImageView imageView;
 
     LocationManager locationManager;
@@ -56,6 +59,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         txtHumidity = (TextView) findViewById(R.id.txtHumidity);
         txtCelsius = (TextView) findViewById(R.id.txtCelsius);
         txtTime = (TextView) findViewById(R.id.txtTime);
+        txtLat = (TextView) findViewById(R.id.txtLat);
         imageView = (ImageView) findViewById(R.id.imageView);
 
 
@@ -83,6 +87,47 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
             return;
         }
         Location location = locationManager.getLastKnownLocation(provider);
+        final Button button = findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+
+                if (imageView.getVisibility() == View.VISIBLE) {
+                    imageView.setVisibility(View.INVISIBLE);
+                    txtCelsius.setTextColor(Color.WHITE);
+                    txtCelsius.setBackgroundColor(Color.BLACK);
+                    txtCity.setTextColor(Color.WHITE);
+                    txtCity.setBackgroundColor(Color.BLACK);
+                    txtTime.setTextColor(Color.WHITE);
+                    txtTime.setBackgroundColor(Color.BLACK);
+                    txtLastUpdate.setTextColor(Color.WHITE);
+                    txtLastUpdate.setBackgroundColor(Color.BLACK);
+                    txtHumidity.setTextColor(Color.WHITE);
+                    txtHumidity.setBackgroundColor(Color.BLACK);
+                    txtDescription.setTextColor(Color.WHITE);
+                    txtDescription.setBackgroundColor(Color.BLACK);
+                    txtLat.setTextColor(Color.WHITE);
+                    txtLat.setBackgroundColor(Color.BLACK);
+
+                } else {
+                    imageView.setVisibility(View.VISIBLE);
+                    txtCelsius.setTextColor(Color.BLACK);
+                    txtCelsius.setBackgroundColor(Color.WHITE);
+                    txtCity.setTextColor(Color.BLACK);
+                    txtCity.setBackgroundColor(Color.WHITE);
+                    txtTime.setTextColor(Color.BLACK);
+                    txtTime.setBackgroundColor(Color.WHITE);
+                    txtLastUpdate.setTextColor(Color.BLACK);
+                    txtLastUpdate.setBackgroundColor(Color.WHITE);
+                    txtHumidity.setTextColor(Color.BLACK);
+                    txtHumidity.setBackgroundColor(Color.WHITE);
+                    txtDescription.setTextColor(Color.BLACK);
+                    txtDescription.setBackgroundColor(Color.WHITE);
+                    txtLat.setTextColor(Color.BLACK);
+                    txtLat.setBackgroundColor(Color.WHITE);
+                }
+            }
+        });
         if (location == null) {
             Log.e("TAG", "No Location");
         }
@@ -179,10 +224,10 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
             txtDescription.setText(String.format("%s",openWeatherMap.getWeather().get(0).getDescription()));
             txtHumidity.setText(String.format("%d%%",openWeatherMap.getMain().getHumidity()));
             txtTime.setText(String.format("%s/%s",Common.unixTimeStampToDateTime(openWeatherMap.getSys().getSunrise()),
-                                                Common.unixTimeStampToDateTime(openWeatherMap.getSys().getSunset())));
+                    Common.unixTimeStampToDateTime(openWeatherMap.getSys().getSunset())));
             txtCelsius.setText(String.format("%.2f °C",openWeatherMap.getMain().getTemp()));
             Picasso.get().load(Common.getImage(openWeatherMap.getWeather().get(0).getIcon())).into(imageView);
-
+            txtLat.setText(String.format("Lat: %s/ Lng: %s",openWeatherMap.getCoord().getLat(),openWeatherMap.getCoord().getLon()));
         }
 
 
